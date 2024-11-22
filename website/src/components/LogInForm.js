839f7d9e4e1787424553a066156306e62134510b
Laxman Yadav <125170892+laxmany9335@users.noreply.github.com>
2024-11-22 16:38:25 +0000

    Login form update

--- website/src/components/LogInForm.js
+++ website/src/components/LogInForm.js
@@ -2,15 +2,13 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { toast } from "react-toastify";
+import PropTypes from "prop-types";
 
-function LogInForm({setIsLoggedIn}) {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
+function LogInForm({ setIsLoggedIn = () => {} }) {
+  const [formData, setFormData] = useState({ email: "", password: "" });
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
+
   function changeHandler(event) {
     setFormData((prevData) => ({
       ...prevData,
@@ -18,14 +16,15 @@ function LogInForm({setIsLoggedIn}) {
     }));
   }
 
-  function submitHandler(event){
+  function submitHandler(event) {
     event.preventDefault();
+    toast.success("Logged In", { autoClose: 3000 });
     setIsLoggedIn(true);
-    toast.success("Logged In");
-    navigate("/dashboard");
-}
+    navigate("/");
+  }
+
   return (
-    <form onSubmit={submitHandler} className="flex flex-col w-full gap-y-4 mt-6 ">
+    <form onSubmit={submitHandler} className="flex flex-col w-full gap-y-4 mt-6">
       {/* Email Field */}
       <label className="w-full">
         <p className="text-[0.875rem] text-[#ced2d9] mb-1 leading-[1.35rem]">
@@ -47,7 +46,7 @@ function LogInForm({setIsLoggedIn}) {
         <p className="text-[0.875rem] text-[#ced2d9] mb-1 leading-[1.35rem]">
           Password <sup className="text-[#f50505]">*</sup>
         </p>
-        <div >
+        <div>
           <input
             required
             type={showPassword ? "text" : "password"}
@@ -55,28 +54,39 @@ function LogInForm({setIsLoggedIn}) {
             onChange={changeHandler}
             placeholder="Enter your password"
             name="password"
-             className="bg-[#6c9db8] w-full rounded-[0.5rem] text-white p-[10px]"
+            className="bg-[#6c9db8] w-full rounded-[0.5rem] text-white p-[10px]"
           />
           <span
             onClick={() => setShowPassword(!showPassword)}
-            className="absolute right-4 top-[42px] cursor-pointer "
+            className="absolute right-4 top-[42px] cursor-pointer"
           >
-            {showPassword ? <AiOutlineEye fontSize={24} fill="#5c6e6a" /> : <AiOutlineEyeInvisible  fontSize={24} fill="#5c6e6a" />}
+            {showPassword ? (
+              <AiOutlineEye fontSize={24} fill="#5c6e6a" />
+            ) : (
+              <AiOutlineEyeInvisible fontSize={24} fill="#5c6e6a" />
+            )}
           </span>
         </div>
         <Link to="#">
-           <p className="text-xs text-[#0a5fc7] mt-1 max-w-max ml-auto">
-             Forgot Password?
-           </p>
+          <p className="text-xs text-[#0a5fc7] mt-1 max-w-max ml-auto">
+            Forgot Password?
+          </p>
         </Link>
       </label>
 
       {/* Submit Button */}
-      <button type="submit"
-      className="bg-yellow-500 rounded-[8px] font-medium text-black-500 px-[12px] py-[8px]"
-      >Sign In</button>
+      <button
+        type="submit"
+        className="bg-yellow-500 rounded-[8px] font-medium text-black-500 px-[12px] py-[8px]"
+      >
+        Sign In
+      </button>
     </form>
   );
 }
+
+LogInForm.propTypes = {
+  setIsLoggedIn: PropTypes.func.isRequired,
+};
 
 export default LogInForm;
